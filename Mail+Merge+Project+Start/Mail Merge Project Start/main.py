e367c43cce819b77636e0a1db7f189780e67694e
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-26 15:12:34 +0800

    Completed code for mail merge

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -6,3 +6,16 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('./Input/Names/invited_names.txt', mode='r') as f:
+    data = f.read()
+
+data = data.split('\n')
+
+with open('./Input/Letters/starting_letter.txt', mode='r') as f:
+    template = f.read()
+
+for person in data:
+    with open(f'./Output/ReadyToSend/Letter_for_{person}.txt', mode='w') as f:
+        new_template = template.replace('[name]', person)
+        f.write(new_template)
